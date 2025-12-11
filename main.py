2b46e9ca6e61d523b887c82ce7a07d03112e555f
rdf-mal <240441674+rdf-mal@users.noreply.github.com>
2025-12-11 15:44:50 +0100

    remove unused prints

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import dev
 
 def main():
-    print("Super toll")
-    print("Next steps ...")
-    print("Now with vscode")
-    print("Another one")
     print("Very important")
 
 def test():
     print("Testing is important")
-    print("I don't like testing")
-
+    
 if __name__ == '__main__':
     main()
     dev.show()
